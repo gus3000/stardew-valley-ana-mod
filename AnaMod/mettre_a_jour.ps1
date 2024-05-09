fe844827e2f9f4699bc36ad78f88fb22da5e9f09
gus3000 <gus3000spam@gmail.com>
2024-05-09 12:16:15 +0200

    fix: auto-update script did not work with several artifacts in release

--- AnaMod/mettre_a_jour.ps1
+++ AnaMod/mettre_a_jour.ps1
@@ -1,14 +1,31 @@
-$gameLocation = "C:\Program Files (x86)\Steam\steamapps\common\Stardew Valley"
-#$gameLocation = "D:\SteamLibrary\steamapps\common\Stardew Valley"
+$gameDefaultLocations = "C:\Program Files (x86)\Steam\steamapps\common\Stardew Valley", "D:\SteamLibrary\steamapps\common\Stardew Valley"
 $releaseZipTempDir = "/tmp/AnaModTmp/"
 
-if ([string]::IsNullOrEmpty($releaseZipTempDir))
+##### Si vous êtes utilisateur de ce script rien modifier en dessous de cette ligne
+$gameLocation = $null
+foreach ($path in $gameDefaultLocations)
+{
+    if (Test-Path -Path $path -PathType Container)
+    {
+        $gameLocation = $path
+        break
+    }
+}
+
+if ($gameLocation -eq $null)
+{
+    Write-Output "Dossier du jeu non trouvé, veuillez le rajouter aux chemins possibles au début du script SVP"
+    exit -1
+}
+
+if ( [string]::IsNullOrEmpty($releaseZipTempDir))
 {
     Write-Output "Il faut un répertoire temporaire, je peux pas bosser moi sinon"
     exit -1
 }
 
-if(Test-Path -Path $releaseZipTempDir -PathType Container) {
+if (Test-Path -Path $releaseZipTempDir -PathType Container)
+{
     Write-Output "Veuillez vérifier que le répertoire $releaseZipTempDir ne contient rien d'important, et supprimez-le svp"
     exit -1
 }
@@ -23,8 +40,25 @@ $latestReleaseInfo = Invoke-RestMethod -Headers $headers -uri https://api.github
 
 $latestReleaseVersion = $latestReleaseInfo.tag_name
 Write-Output "Dernière version : $latestReleaseVersion"
-$releaseZipUrl = $latestReleaseInfo.assets.browser_download_url
+$releaseArtifactsUrl = $latestReleaseInfo.assets.browser_download_url
 
+$releaseZipUrl = $null
+foreach ($url in $releaseArtifactsUrl)
+{
+    Write-Output "on check $url"
+    if ($url -match "AnaMod.*.zip")
+    {
+        $releaseZipUrl = $url
+    }
+}
+
+if ($releaseZipUrl -eq $null)
+{
+    Write-Output "Archive du mod non trouvée :("
+    exit -1
+}
+
+Write-Output "Téléchargement de $releaseZipUrl..."
 $releaseZipTempLocation = "$releaseZipTempDir/anamod.zip"
 Invoke-WebRequest $releaseZipUrl -OutFile $releaseZipTempLocation
 Expand-Archive $releaseZipTempLocation -DestinationPath $releaseZipTempDir
@@ -32,7 +66,7 @@ Expand-Archive $releaseZipTempLocation -DestinationPath $releaseZipTempDir
 $modsFolder = "$gameLocation/Mods"
 $AnaModFolder = "$modsFolder/AnaMod"
 
-if(Test-Path -Path $AnaModFolder -PathType Container)
+if (Test-Path -Path $AnaModFolder -PathType Container)
 {
     rm -Recurse $AnaModFolder
 }
